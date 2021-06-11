e1c2d01a093d9770474def92e9de2f41a7f5cdda
Aryanti Kasim <aryantikasim10@gmail.com>
2021-06-12 01:14:06 +0800

    Update Database

--- app/src/main/java/id/ac/unhas/crudtodolist/db/note/Note.kt
+++ app/src/main/java/id/ac/unhas/crudtodolist/db/note/Note.kt
@@ -4,7 +4,9 @@ import android.os.Parcelable
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import kotlinx.android.parcel.Parcelize
 
+@Parcelize
 @Entity(tableName = "note")
 data class Note(
     @PrimaryKey(autoGenerate = true)
